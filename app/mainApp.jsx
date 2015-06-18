fc64d8feab05d9d69f6e8be8fa75c22f80a3bc0c
Saul Shanabrook <s.shanabrook@gmail.com>
2015-06-18 15:16:05 -0400

    add TBS support (addresses #6)

--- app/mainApp.jsx
+++ app/mainApp.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import {root} from "baobab-react/decorators";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+
 import Tree from "./tree";
 import StateList from "./components/StateList";
+
 
 @root(Tree)
 class Application extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <StateList></StateList>
       </div>
     );
