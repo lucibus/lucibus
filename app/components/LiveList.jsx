e2248f857abb00fa1a92391e9fbae820a47e0266
Saul Shanabrook <s.shanabrook@gmail.com>
2015-06-19 20:25:56 -0400

    add example of sub component

--- app/components/LiveList.jsx
+++ app/components/LiveList.jsx
@@ -2,19 +2,27 @@ import React, {Component} from "react";
 import {branch} from "baobab-react/decorators";
 
 import styles from "./LiveList.css";
+import SystemItem from "./SystemItem"
 
 
 @branch({
   cursors: {
-    level: ["live", "level"],
+    live: ["live"]
   }
 })
 class LiveList extends Component {
   render() {
     return (
-      <p className={styles.level}>
-        We are currently at level {this.props.level}
-      </p>
+      <div className={styles.level}>
+        <p className={styles.level}>
+          We are currently at level {this.props.live.level}
+        </p>
+        <ol>
+          {this.props.live.systems.map(function(system) {
+            return <li><SystemItem {...system}></SystemItem></li>;
+          })}
+        </ol>
+      </div>
     );
 
   }
---
+++ app/components/SystemItem.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+
+class SystemItem extends Component {
+  render() {
+    return (
+      <p>
+        This is a {this.props.type} curretly at level {this.props.level}
+      </p>
+    );
+
+  }
+}
+
+export default SystemItem;
+
